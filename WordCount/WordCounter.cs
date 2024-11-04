a8ad4e338147a8ab9201dc76205c50dae1fc8926
Kishko0 <yousefkasas506@gmail.com>
2024-11-04 10:17:08 +0100

    Add project files.

---
+++ WordCount/WordCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace WordCount
+{
+    internal class WordCounter
+    {
+        public int CountWordOccurrences(string text, string word)
+        {
+            int count = 0;
+
+            text = text.ToLower();
+            word = word.ToLower();
+
+            string[] words = text.Split(new char[] { ' ', '.', ',', ';', ':', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string w in words)
+            {
+                if (w.Contains(word))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
